5815d5442b3a31163880d43c7f1da86e2086d8b0
Eliasu Abraman <e.abraman@inpsyde.com>
2024-11-24 17:46:26 +0000

    Add routes for tasks

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskListController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -27,12 +28,34 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
+
     Route::get('/task-list', [TaskListController::class, 'index'])->middleware(['auth', 'verified'])->name('task-list');
     Route::get('/task-list/create', [TaskListController::class, 'create'])->middleware(['auth', 'verified'])->name('task-list.create');
-    Route::put('/task-list/:id/edit', [TaskListController::class, 'edit'])->middleware(['auth', 'verified'])->name('task-list.edit');
-    Route::delete('/task-list/:id/delete', [TaskListController::class, 'destroy'])->middleware(['auth', 'verified'])->name('task-list.destroy');
+    Route::get('/task-list/{task_list_id}/share', [TaskListController::class, 'share'])->middleware(['auth', 'verified'])->name('task-list.share');
+    Route::get('/task-list/{task_list_id}/show', [TaskListController::class, 'show'])->middleware(['auth', 'verified'])->name('task-list.show');
+    Route::get('/task-list/{task_list_id}/edit', [TaskListController::class, 'edit'])->middleware(['auth', 'verified'])->name('task-list.edit');
+
+    Route::post('/task-list/store', [TaskListController::class, 'store'])->middleware(['auth', 'verified'])->name('task-list.store');
+
+    Route::put('/task-list/{task_list_id}/update', [TaskListController::class, 'update'])->middleware(['auth', 'verified'])->name('task-list.update');
+    Route::patch('/task-list/{task_list_id}/share', [TaskListController::class, 'processShare'])->middleware(['auth', 'verified'])->name('task-list.share.process');
+
+    Route::delete('/task-list/{id}/delete', [TaskListController::class, 'destroy'])->middleware(['auth', 'verified'])->name('task-list.destroy');
 });
 
+Route::middleware('auth')->group(function () {
+
+    Route::get('/task/create', [TaskController::class, 'create'])->middleware(['auth', 'verified'])->name('task.create');
+    Route::get('/task/{task_id}/show', [TaskController::class, 'show'])->middleware(['auth', 'verified'])->name('task.show');
+    Route::get('/task/{task_id}/edit', [TaskController::class, 'edit'])->middleware(['auth', 'verified'])->name('task.edit');
+    Route::get('/tasks', [TaskController::class, 'index'])->middleware(['auth', 'verified'])->name('tasks');
+
+    Route::post('/task/store', [TaskController::class, 'store'])->middleware(['auth', 'verified'])->name('task.store');
+
+    Route::put('/task/{task_id}/update', [TaskController::class, 'update'])->middleware(['auth', 'verified'])->name('task.update');
+
+    Route::delete('/task/{task_id}/delete', [TaskController::class, 'destroy'])->middleware(['auth', 'verified'])->name('task.destroy');
+});
 
 Route::get('/users', [RegisteredUserController::class, 'index'])->middleware(['auth', 'verified'])->name('users');
 
